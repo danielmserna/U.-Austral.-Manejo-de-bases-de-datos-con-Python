8a7b0eb29ee56b9f451908afe67f3ef5b9c00609
Daniel Serna <35707534+danielmserna@users.noreply.github.com>
2020-11-07 16:58:11 -0500

    Add files via upload

---
+++ 9_Mapeo_de_modelos.py
@@ -0,0 +1,25 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Sequence
+
+engine = create_engine('sqlite:///:memory:')
+
+Base = declarative_base()
+
+class Author(Base):
+    __tablename__ = 'author'
+
+    id = Column(Integer, Sequence('author_id_seq'), primary_key = True)
+    firstname = Column(String)
+    lastname = Column(String)
+
+    def __repr__(self):
+        return "{} {}".format(self.firstname, self.lastname)
+
+Author.__table__
+Base.metadata.create_all(engine)
+
+author = Author(firstname = 'Joanne', lastname = 'Rowling')
+author.firstname
+author.lastname
+author.id
